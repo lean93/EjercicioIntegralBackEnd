a59b89008000e2f5c3ebf30eab8ec4b5d1546c3b
lean93 <leandro.rodriguez@despegar.com>
2016-02-28 18:10:23 -0300

    Empiezo con el logger del ejercicio, falta configuracion

--- src/Compradores/Cliente.java
+++ src/Compradores/Cliente.java
@@ -7,42 +7,57 @@ import java.util.List;
 import Paquetes.*;
 import Excepciones.*;
 
+import org.apache.logging.log4j.*;
+
 public class Cliente {
+
+	private static final Logger logger = LogManager.getLogger();
 
 	private double saldo;
 	private List<Paquete> compras;
-
+	
+	
 	public Cliente(double unSaldo) {
 		saldo = unSaldo;
 		compras = new ArrayList<Paquete>();
 	}
 	public Collection<Paquete> getPaquetesComprados(){
+		
 		return compras;
 	}
 
 	public void comprarPaquete(Paquete unPaquete) throws FalloEnCompraExcepcion{
 		
 		try{
+			logger.info("Realizando Compra");
 		realizarPago(unPaquete.getPrecioPaquete(this));
 		añadirPaquete(unPaquete);
 		
+		logger.info("Compra Realizada Exitosamente");
+		
 		} catch(SinSaldoException e){
 			System.out.println(e.getMessage());
+			logger.error("Fallo en la Compra");
 			throw new FalloEnCompraExcepcion("Hubo un problema en la compra");
+			
 		} finally{
+			logger.info("Mostrando Saldo del Cliente");
 			System.out.println("saldo del Cliente: " + getSaldo() );
 		}
 	}
 	
 	public void realizarPago(double unMonto) throws SinSaldoException{
 		if (saldo > unMonto){
+			logger.info("Realizando Pago");
 			saldo = saldo - unMonto;
-		} else {throw new SinSaldoException("Te quedaste sin saldo pibe !!");}
+		} else {
+			logger.error("Saldo insuficiente para realizar el pago");
+			throw new SinSaldoException("Te quedaste sin saldo pibe !!");}
 		
 	}
 	
 	public void añadirPaquete(Paquete unPaquete){
-		
+		logger.info("Añadiendo el paquete a Compras");
 		compras.add(unPaquete);
 	}
 
@@ -67,7 +82,7 @@ public class Cliente {
 
 		Paquete masCaro = new Paquete();
 		double mayor = 0;
-
+		logger.info("Buscando el Paquete mas Caro");
 		for (Paquete unPaquete : unaListaDePaquetes) {
 
 			if (unPaquete.getPrecioPaquete(this) > mayor) {
@@ -75,6 +90,7 @@ public class Cliente {
 				mayor = unPaquete.getPrecioPaquete(this);
 			}
 		}
+		logger.info("Paquete mas caro encontrado");
 		return masCaro;
 	}
 	
