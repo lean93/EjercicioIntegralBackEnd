0d1efda2639cdb84d9b292b37564078a0e7bbec6
lean93 <leanrodri2093@gmail.com>
2016-03-01 23:49:06 -0300

    primer test con paquetes mockeados

--- src/Tests/TestEjercicio1.java
+++ src/Tests/TestEjercicio1.java
@@ -8,11 +8,17 @@ import org.junit.Test;
 import Compradores.*;
 import Excepciones.FalloEnCompraExcepcion;
 import Paquetes.*;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 public class TestEjercicio1 {
 	
 	private Individuo leandro;
 	private Paquete paquete1;
+	private Paquete paqueteMock;
+	private Paquete paqueteMock2;
+	private Paquete paqueteMardelMock;
 	private Paquete paquete2;
 	private Paquete paqueteMardel;
 	private Paquete paqueteDeImpuesto;
@@ -21,6 +27,9 @@ public class TestEjercicio1 {
 	public void setUp(){
 	
 		 leandro = new Individuo(2000);
+		 paqueteMock = mock(Paquete.class);
+		 paqueteMock2 = mock(Paquete.class);
+		 paqueteMardelMock = mock(Paquete.class);
 		 paquete1 = new Paquete(1.25, new PrecioFijo(500));
 		 paquete2 = new Paquete(1.1,new PrecioFijo(100));
 		 paqueteMardel = new Paquete(1.2, new SegunHabitacion(2, 50));
@@ -38,6 +47,15 @@ public class TestEjercicio1 {
 		leandro.comprarPaquete(paquete1);
 		assertEquals(1375.0,leandro.getSaldo(),0.01);
 	}
+	
+	@Test
+	public void leanCompraPaqueteMockYQuedaCon1375DeSaldo() throws FalloEnCompraExcepcion{
+		when(paqueteMock.getPrecioPaquete(leandro)).thenReturn(625.0);
+		leandro.comprarPaquete(paqueteMock);
+		assertEquals(1375.0, leandro.getSaldo(), 0.01);
+		verify(paqueteMock,times(1)).getPrecioPaquete(leandro);
+	}
+	
 	
 	@Test 
 	public void leanCompraPaquete2YQuedaCon1890DeSaldo() throws FalloEnCompraExcepcion{
